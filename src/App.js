3a7effa9af2bba8194eb2c2cf593dbf389b1e8d6
OlgaBeliaeva <olgabel505@web.de>
2024-10-01 19:53:13 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import React from 'react';
+import Rating from './components/Rating';
+import List from './components/List';
 
 function App() {
   return (
-    <div >
-      
+    <div className="App">
+      <div style={{ marginBottom: '40px' }}>
+        <Rating />
+      </div>
+      <div style={{ marginTop: '40px' }}>
+        <List />
+      </div>
     </div>
   );
 }
