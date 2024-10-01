2568ed894d99d3e75f543cc34011202935ffefed
OlgaBeliaeva <olgabel505@web.de>
2024-10-01 19:58:44 +0200

    end

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/List.css'; // Импортируем файл стилей
+import '../styles/List.css'; 
 
 const List = () => {
   const [people, setPeople] = useState([
